b5c91cc3891bd9fdd8737cf8a68789855e085b54
nobrain3 <todyl@naver.com>
2026-01-24 16:11:17 +0900

    53 : 설정 화면 메인 UI 구현

---
+++ youtube_edu_controller/lib/views/screens/grade_settings_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class GradeSettingsScreen extends StatelessWidget {
+  const GradeSettingsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('나이/학년 설정'),
+      ),
+      body: const Center(
+        child: Text('나이/학년 설정 화면 - 준비 중'),
+      ),
+    );
+  }
+}
